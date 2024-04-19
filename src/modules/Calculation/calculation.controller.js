af8f9db7153ae49c3bfa0bd8a37e6e704a2ff22f
FirstName <smd71430@gmail.com>
2024-04-19 18:32:16 +0600

    get Total Balance

--- src/modules/Calculation/calculation.controller.js
+++ src/modules/Calculation/calculation.controller.js
@@ -29,9 +29,132 @@ const getTotalSalesAmount = async (req, res) => {
     res.status(200).json({ totalBalance });
   });
 };
+const getTotalPurchaseAmount = async (req, res) => {
+  useCalculate("purchase", "actual_amount", (err, totalBalance) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    res.status(200).json({ totalBalance });
+  });
+};
+const getTotalAccountsPayableAmount = async (req, res) => {
+  useCalculate("accounts_payable", "actual_amount", (err, totalBalance) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    res.status(200).json({ totalBalance });
+  });
+};
+const getTotalAccountsReceivableAmount = async (req, res) => {
+  useCalculate("accounts_receivable", "actual_amount", (err, totalBalance) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    res.status(200).json({ totalBalance });
+  });
+};
+const getTotalFixedAssetsAmount = async (req, res) => {
+  useCalculate("fixed_assets", "actual_amount", (err, totalBalance) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    res.status(200).json({ totalBalance });
+  });
+};
+
+const getTotalBalance = async (req, res) => {
+  let totalBalance = 0;
+
+  // Get total accounts balance
+  useCalculate("accounts", "balance", (err, accountsBalance) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    totalBalance += accountsBalance;
+
+    // Get total sales amount
+    useCalculate("sales", "actual_amount", (err, salesAmount) => {
+      if (err) {
+        res.status(500).json(err);
+        return;
+      }
+      totalBalance += salesAmount;
+
+      // Get total accounts payable amount
+      useCalculate(
+        "accounts_payable",
+        "actual_amount",
+        (err, accountsPayableAmount) => {
+          if (err) {
+            res.status(500).json(err);
+            return;
+          }
+          totalBalance -= accountsPayableAmount;
+
+          // Get total purchases amount
+          useCalculate("purchase", "actual_amount", (err, purchaseAmount) => {
+            if (err) {
+              res.status(500).json(err);
+              return;
+            }
+            totalBalance -= purchaseAmount;
+
+            // Get total expenses amount
+            useCalculate("expense", "actual_amount", (err, expenseAmount) => {
+              if (err) {
+                res.status(500).json(err);
+                return;
+              }
+              totalBalance -= expenseAmount;
+
+              // Get total accounts receivable amount
+              useCalculate(
+                "accounts_receivable",
+                "actual_amount",
+                (err, accountsReceivableAmount) => {
+                  if (err) {
+                    res.status(500).json(err);
+                    return;
+                  }
+                  totalBalance += accountsReceivableAmount;
+
+                  // Get total fixed assets amount
+                  useCalculate(
+                    "fixed_assets",
+                    "actual_amount",
+                    (err, fixedAssetsAmount) => {
+                      if (err) {
+                        res.status(500).json(err);
+                        return;
+                      }
+                      totalBalance += fixedAssetsAmount;
+
+                      // Send the final total balance as response
+                      res.status(200).json({ totalBalance });
+                    }
+                  );
+                }
+              );
+            });
+          });
+        }
+      );
+    });
+  });
+};
 
 module.exports = {
+  getTotalBalance,
   getTotalBankAmount,
-  getTotalExpenseAmount,
   getTotalSalesAmount,
+  getTotalExpenseAmount,
+  getTotalPurchaseAmount,
+  getTotalFixedAssetsAmount,
+  getTotalAccountsPayableAmount,
+  getTotalAccountsReceivableAmount,
 };
--- src/modules/Calculation/calculation.routes.js
+++ src/modules/Calculation/calculation.routes.js
@@ -2,11 +2,28 @@ const express = require("express");
 const calculationController = require("./calculation.controller");
 const router = express.Router();
 
+router.get("/total-balance", calculationController.getTotalBalance);
 router.get("/total-bank-amount", calculationController.getTotalBankAmount);
 router.get(
   "/total-expense-amount",
   calculationController.getTotalExpenseAmount
 );
 router.get("/total-sales-amount", calculationController.getTotalSalesAmount);
+router.get(
+  "/total-fixed-assets-amount",
+  calculationController.getTotalFixedAssetsAmount
+);
+router.get(
+  "/total-accounts-payable-amount",
+  calculationController.getTotalAccountsPayableAmount
+);
+router.get(
+  "/total-accounts-receivable-amount",
+  calculationController.getTotalAccountsReceivableAmount
+);
+router.get(
+  "/total-purchase-amount",
+  calculationController.getTotalPurchaseAmount
+);
 
 module.exports = { calculationRoutes: router };
