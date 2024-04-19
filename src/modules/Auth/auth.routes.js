12e0dc1d562064ed3155d6a9c5acef73b5626a6e
sabbirsami <smd71430u@gmail.com>
2024-04-19 14:59:57 +0600

    updateProfileData using patch method

--- src/modules/Auth/auth.routes.js
+++ src/modules/Auth/auth.routes.js
@@ -3,5 +3,6 @@ const authController = require("./auth.controller");
 const router = express.Router();
 
 router.post("/", authController.loginUser);
+router.patch("/:id", authController.updateProfileData);
 
 module.exports = { authRoutes: router };
