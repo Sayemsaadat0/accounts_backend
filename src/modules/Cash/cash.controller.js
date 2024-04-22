727d6cb467499a3f4cc20c5cd87504170dce3281
Sayemsaadat0 <sayemsaadat0@gmail.com>
2024-04-22 15:45:33 +0600

    cash

--- src/modules/Cash/cash.controller.js
+++ src/modules/Cash/cash.controller.js
@@ -1,55 +1,108 @@
 const { connection } = require("../../config");
+const generateUniqueId = require("../../middleware/generateUniqueId");
 
-const getAllCashRecords = async (req, res) => {
+const getAllCash = async (req, res) => {
   const sql = "SELECT * FROM cash";
   connection.query(sql, (err, results) => {
     if (err) {
-      console.error("Error getting cash records: " + err.message);
-      res.status(500).json({ error: "Error getting cash records" });
+      console.error("Error getting cash data: " + err.message);
+      res.status(500).json({ error: "Error getting cash data" });
       return;
     }
     res.status(200).json(results);
   });
 };
 
-const createCashRecord = async (req, res) => {
-  const { amount, createdAt } = req.body;
-  const sql = "INSERT INTO cash (amount, createdAt) VALUES (?, ?)";
-  connection.query(sql, [amount, createdAt], (err, result) => {
-    if (err) {
-      console.error("Error creating cash record: " + err.message);
-      res.status(500).json({ error: "Error creating cash record" });
-      return;
-    }
-    res.status(201).json({
-      message: "Cash record created successfully",
-      cashId: result.insertId,
+const createCash = async (req, res) => {
+  const uniqueId = generateUniqueId();
+  const { amount } = req.body;
+  const checkSql = "SELECT COUNT(*) AS count FROM cash";
+  connection.query(checkSql, (err, result) => {
+
+    // if (err) {
+    //   console.error("Error checking cash data: " + err.message);
+    //   res.status(500).json({ error: "Error checking cash data" });
+    //   return;
+    // }
+    // If there is already cash data, return an error
+    // if (result[0].count > 0) {
+    //   res.status(400).json({ error: "Cash data already exists" });
+    //   return;
+    // }
+
+    const sql = "INSERT INTO cash (id, amount) VALUES (?, ?)";
+    connection.query(sql, [uniqueId, amount], (err, result) => {
+      if (err) {
+        console.error("Error creating cash data: " + err.message);
+        res.status(500).json({ error: "Error creating cash data" });
+        return;
+      }
+      res.status(201).json({
+        message: "Cash data created successfully",
+        cashId: result.insertId,
+      });
     });
   });
 };
 
-const updateCashRecord = async (req, res) => {
+const getCashById = async (req, res) => {
   const cashId = req.params.id;
-  const { amount, createdAt } = req.body;
-  const sql = "UPDATE cash SET amount = ?, createdAt = ? WHERE id = ?";
-  connection.query(sql, [amount, createdAt, cashId], (err, result) => {
+  const sql = "SELECT * FROM cash WHERE id = ?";
+  connection.query(sql, [cashId], (err, result) => {
     if (err) {
-      console.error("Error updating cash record: " + err.message);
-      res.status(500).json({ error: "Error updating cash record" });
+      console.error("Error getting cash data: " + err.message);
+      res.status(500).json({ error: "Error getting cash data" });
       return;
     }
-    if (result.affectedRows === 0) {
-      res.status(404).json({ message: "Cash record not found" });
+    if (result.length === 0) {
+      res.status(404).json({ message: "Cash data not found" });
       return;
     }
-    res.status(200).json({ message: "Cash record updated successfully" });
+    res.status(200).json(result[0]);
   });
 };
 
- const cashController = {
-  getAllCashRecords,
-  updateCashRecord,
-  createCashRecord,
+const deleteCash = async (req, res) => {
+  const cashId = req.params.id;
+  const sql = "DELETE FROM cash WHERE id = ?";
+  connection.query(sql, [cashId], (err, result) => {
+    if (err) {
+      console.error("Error deleting cash data: " + err.message);
+      res.status(500).json({ error: "Error deleting cash data" });
+      return;
+    }
+    if (result.affectedRows === 0) {
+      res.status(404).json({ message: "Cash data not found" });
+      return;
+    }
+    res.status(200).json({ message: "Cash data deleted successfully" });
+  });
+};
+
+const updateCash = async (req, res) => {
+  const cashId = req.params.id;
+  const { amount } = req.body;
+  const sql = "UPDATE cash SET amount = ? WHERE id = ?";
+  connection.query(sql, [amount, cashId], (err, result) => {
+    if (err) {
+      console.error("Error updating cash data: " + err.message);
+      res.status(500).json({ error: "Error updating cash data" });
+      return;
+    }
+    if (result.affectedRows === 0) {
+      res.status(404).json({ message: "Cash data not found" });
+      return;
+    }
+    res.status(200).json({ message: "Cash data updated successfully" });
+  });
+};
+
+const cashController = {
+  getAllCash,
+  createCash,
+  updateCash,
+  deleteCash,
+  getCashById,
 };
 
 module.exports = cashController;
--- src/modules/Cash/cash.router.js
+++ src/modules/Cash/cash.router.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cashController = require("./cash.controller");
 const router = express.Router();
 
-router.get("/", cashController.getAllCashRecords);
-router.post("/", cashController.createCashRecord);
-
-router.put("/:id", cashController.updateCashRecord);
+router.get("/", cashController.getAllCash);
+router.post("/", cashController.createCash);
+router.put("/:id", cashController.updateCash);
+router.delete("/:id", cashController.deleteCash);
+router.get("/:id", cashController.getCashById);
 
 module.exports = { cashRoutes: router };
