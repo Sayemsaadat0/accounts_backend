8943231ff8fc8764875667f430629ee1564c3a7a
FirstName <smd71430@gmail.com>
2024-04-23 17:59:14 +0600

    making hook

--- src/hook/useManageBankAccount.js
+++ src/hook/useManageBankAccount.js
@@ -1,42 +1,74 @@
-const useManageBankAccount = (id, amount) => {
-  const {
-    select_date,
+const useManageBankAccount = ({ id, data, amount, sql, transaction_type }) => {
+  const uniqueId = generateUniqueId();
+
+  const formattedDate = new Date(select_date).toISOString().split("T")[0];
+  const sql = sql;
+
+  const values = [
+    uniqueId,
+    formattedDate,
     payment_type,
     actual_amount,
     paid_amount,
     due_amount,
     note,
-    ledger_name,
+    JSON.stringify(ledger_name),
     company_name,
     project_name,
-  } = req.body;
-  const sql =
-    "UPDATE expense SET select_date = ?, payment_type = ?, actual_amount = ?, paid_amount = ?, due_amount = ?, note = ?, ledger = ?, company_name = ?, project_name = ?, WHERE id = ?";
-  connection.query(
-    sql,
-    [
-      select_date,
-      payment_type,
-      actual_amount,
-      paid_amount,
-      due_amount,
-      note,
-      JSON.stringify(ledger_name),
-      company_name,
-      project_name,
-      id,
-    ],
-    (err, result) => {
-      if (err) {
-        console.error("Error updating expense: " + err.message);
-        res.status(500).json({ error: "Error updating expense" });
-        return;
-      }
-      if (result.affectedRows === 0) {
-        res.status(404).json({ message: "Expense not found" });
-        return;
-      }
-      res.status(200).json({ message: "Expense updated successfully" });
+  ];
+
+  // Deduct expense from the selected account balance
+  let updateOperator = "-";
+  if (
+    transaction_type === "income" ||
+    transaction_type === "account_Recivable" ||
+    transaction_type === "sales"
+  ) {
+    updateOperator = "+";
+  }
+  console.log("Update Operator:", updateOperator);
+
+  const updateBalanceSql = `UPDATE accounts SET balance = balance ${updateOperator} ? WHERE id = ?`;
+  const updateBalanceValues = [actual_amount, account_id];
+  console.log("Update Balance SQL:", updateBalanceSql);
+  console.log("Update Balance Values:", updateBalanceValues);
+
+  connection.beginTransaction(function (err) {
+    if (err) {
+      console.error("Error starting transaction: " + err.message);
+      res.status(500).json({ error: "Error creating expense" });
+      return;
     }
-  );
+
+    connection.query(sql, values, function (err, result) {
+      if (err) {
+        return connection.rollback(function () {
+          console.error("Error creating expense: " + err.message);
+          res.status(500).json({ error: "Error creating expense" });
+        });
+      }
+
+      connection.query(updateBalanceSql, updateBalanceValues, function (err) {
+        if (err) {
+          return connection.rollback(function () {
+            console.error("Error updating account balance: " + err.message);
+            res.status(500).json({ error: "Error creating expense" });
+          });
+        }
+
+        connection.commit(function (err) {
+          if (err) {
+            return connection.rollback(function () {
+              console.error("Error committing transaction: " + err.message);
+              res.status(500).json({ error: "Error creating expense" });
+            });
+          }
+          res.status(201).json({
+            message: "Expense created successfully",
+            expenseId: result.insertId,
+          });
+        });
+      });
+    });
+  });
 };
--- src/modules/Expenses/expenses.controller.js
+++ src/modules/Expenses/expenses.controller.js
@@ -33,61 +33,6 @@ const createExpense = async (req, res) => {
     company_name,
     project_name,
   ];
-
-  // Deduct expense from the selected account balance
-  let updateOperator = "-";
-  if (
-    transaction_type === "income" ||
-    transaction_type === "account_Recivable" ||
-    transaction_type === "sales"
-  ) {
-    updateOperator = "+";
-  }
-  console.log("Update Operator:", updateOperator);
-
-  const updateBalanceSql = `UPDATE accounts SET balance = balance ${updateOperator} ? WHERE id = ?`;
-  const updateBalanceValues = [actual_amount, account_id];
-  console.log("Update Balance SQL:", updateBalanceSql);
-  console.log("Update Balance Values:", updateBalanceValues);
-
-  connection.beginTransaction(function (err) {
-    if (err) {
-      console.error("Error starting transaction: " + err.message);
-      res.status(500).json({ error: "Error creating expense" });
-      return;
-    }
-
-    connection.query(sql, values, function (err, result) {
-      if (err) {
-        return connection.rollback(function () {
-          console.error("Error creating expense: " + err.message);
-          res.status(500).json({ error: "Error creating expense" });
-        });
-      }
-
-      connection.query(updateBalanceSql, updateBalanceValues, function (err) {
-        if (err) {
-          return connection.rollback(function () {
-            console.error("Error updating account balance: " + err.message);
-            res.status(500).json({ error: "Error creating expense" });
-          });
-        }
-
-        connection.commit(function (err) {
-          if (err) {
-            return connection.rollback(function () {
-              console.error("Error committing transaction: " + err.message);
-              res.status(500).json({ error: "Error creating expense" });
-            });
-          }
-          res.status(201).json({
-            message: "Expense created successfully",
-            expenseId: result.insertId,
-          });
-        });
-      });
-    });
-  });
 };
 
 const getAllExpenses = async (req, res) => {
